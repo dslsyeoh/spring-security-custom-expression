bbadcbc489f9068906da276440d98f2260683c7b
dslsyeoh <syeoh91x@gmail.com>
2019-10-27 23:43:22 +0800

    add implementation for loadUserByUsername

--- src/main/java/com/dsl/spring/profile/practice/handlers/UserServiceHandler.java
+++ src/main/java/com/dsl/spring/profile/practice/handlers/UserServiceHandler.java
@@ -5,17 +5,25 @@
 
 package com.dsl.spring.profile.practice.handlers;
 
+import com.dsl.spring.profile.practice.domain.UserEntity;
 import com.dsl.spring.profile.practice.dto.User;
+import com.dsl.spring.profile.practice.repository.UserRepository;
 import com.dsl.spring.profile.practice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserServiceHandler implements UserService
 {
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public List<User> list()
     {
@@ -23,7 +31,7 @@ public class UserServiceHandler implements UserService
     }
 
     @Override
-    public String search(Long id)
+    public User search(Long id)
     {
         return null;
     }
@@ -35,8 +43,13 @@ public class UserServiceHandler implements UserService
     }
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException
+    public UserDetails loadUserByUsername(String username)
     {
-        return null;
+        UserEntity entity = userRepository.findByUsername(username);
+        if(Objects.isNull(entity))
+        {
+            throw new UsernameNotFoundException("username not found");
+        }
+        return new org.springframework.security.core.userdetails.User(entity.getUsername(), entity.getPassword(), Collections.emptyList());
     }
 }
--- src/main/java/com/dsl/spring/profile/practice/repository/UserRepository.java
+++ src/main/java/com/dsl/spring/profile/practice/repository/UserRepository.java
@@ -6,10 +6,12 @@
 package com.dsl.spring.profile.practice.repository;
 
 import com.dsl.spring.profile.practice.domain.UserEntity;
+import com.dsl.spring.profile.practice.dto.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<Long, UserEntity>
 {
+    UserEntity findByUsername(String username);
 }
