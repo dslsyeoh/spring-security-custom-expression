31cc8b461f8a686bfd5f2b6fcc814ce19cc3bac1
dslsyeoh <syeoh91x@gmail.com>
2019-10-27 23:19:14 +0800

    setup security

---
+++ src/main/java/com/dsl/spring/profile/practice/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,25 @@
+/*
+ * Author Steven Yeoh
+ * Copyright (c) 2019. All rights reserved
+ */
+
+package com.dsl.spring.profile.practice.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint
+{
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException
+    {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "unauthorized");
+    }
+}
---
+++ src/main/java/com/dsl/spring/profile/practice/security/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+/*
+ * Author Steven Yeoh
+ * Copyright (c) 2019. All rights reserved
+ */
+
+package com.dsl.spring.profile.practice.security;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter
+{
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException
+    {
+        filterChain.doFilter(request, response);
+    }
+}
