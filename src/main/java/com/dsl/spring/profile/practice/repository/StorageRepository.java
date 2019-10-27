f59396dd3b6e2389d74cc1631dc519a129abeb2a
dslsyeoh <syeoh91x@gmail.com>
2019-10-27 21:17:31 +0800

    setup Repositories

---
+++ src/main/java/com/dsl/spring/profile/practice/repository/StorageRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Author Steven Yeoh
+ * Copyright (c) 2019. All rights reserved
+ */
+
+package com.dsl.spring.profile.practice.repository;
+
+import com.dsl.spring.profile.practice.domain.StorageEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StorageRepository extends JpaRepository<Long, StorageEntity>
+{
+}
