8010bc63f61256e4f6a995a7f9569829407533f1
dslsyeoh <syeoh91x@gmail.com>
2019-10-29 01:14:36 +0800

    add implementation for TokenService

--- src/main/java/com/dsl/spring/profile/practice/handlers/TokenServiceHandler.java
+++ src/main/java/com/dsl/spring/profile/practice/handlers/TokenServiceHandler.java
@@ -7,14 +7,26 @@ package com.dsl.spring.profile.practice.handlers;
 
 import com.dsl.spring.profile.practice.dto.User;
 import com.dsl.spring.profile.practice.service.TokenService;
+import com.dsl.spring.profile.practice.utils.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TokenServiceHandler implements TokenService
 {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
     @Override
     public String generateToken(User user)
     {
-        return "generated token";
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        return jwtTokenUtil.generateToken(authentication);
     }
 }
